cf829525fa4d0c056f1193f8f86a809c393ac4ea
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-02-18 16:19:21 -0600

    uncommented build for prod

--- server/server.js
+++ server/server.js
@@ -45,9 +45,9 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // comment this out to use the graphql playground
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
